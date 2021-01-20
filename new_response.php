6f8dd7d3dc624c0f4effbc9aa7e67dc26a7b7a52
Argho Chakraborty <arghorkm35@gmail.com>
2021-01-20 21:45:44 +0530

    Update new_response.php

--- new_response.php
+++ new_response.php
@@ -1,17 +1,17 @@
 <?php
 	if(isset($_POST['submit'])){
-        $fname=$_POST['first name'];
-        $lname=$_POST['last name']
-		$email=$_POST['email id'];
+        $fname=$_POST['fname'];
+        $lname=$_POST['lname'];
+		$email=$_POST['email'];
 		$country=$_POST['country'];
         $subject=$_POST['subject'];
 
 		$to='arghorkm35@gmail.com'; 
-		$subject='Form Submission';
-		$message="Name :".$fname .$lname"\n"."Country :".$country."\n"."Wrote the following :"."\n\n".$subject;
+		$msg='Form Submission';
+	    $message="First Name :".$fname. "Last Name :".$lname. "\n"."Country :".$country."\n"."Wrote the following: ".$subject;
         $headers="From: ".$email;
 
-		if(mail($to, $subject, $message, $headers)){
+		if(mail($to, $msg, $message, $headers)){
 			echo "<h1>Sent Successfully! Thank you"." ".$fname.", We will contact you shortly!</h1>";
 		}
 		else{
